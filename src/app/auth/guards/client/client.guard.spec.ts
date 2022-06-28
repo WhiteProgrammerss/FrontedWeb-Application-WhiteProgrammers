210c0f98c94a91af2c3d06a33c8cc25c033550e8
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2022-06-28 16:56:16 -0500

    add client guard

---
+++ src/app/auth/guards/client/client.guard.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import {ClientGuard} from "./client.guard";
+
+
+
+describe('ClientGuardGuard', () => {
+  let guard: ClientGuard;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    guard = TestBed.inject(ClientGuard);
+  });
+
+  it('should be created', () => {
+    expect(guard).toBeTruthy();
+  });
+});
